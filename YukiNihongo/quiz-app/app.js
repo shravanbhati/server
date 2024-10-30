620bb70305f7a93e04369efa924e0f92a204ed36
Shravan Bhati <153543313+shravanbhati@users.noreply.github.com>
2024-10-30 05:42:57 +0530

    Update app.js

--- YukiNihongo/quiz-app/app.js
+++ YukiNihongo/quiz-app/app.js
@@ -143,7 +143,7 @@ const showQuizSummary = () => {
         
         // Set the HTML for this question item with conditional styling and detailed answer data
         questionItem.innerHTML = `
-            <p style="color: var(--bodyBg); font-weight: 600;">Q${index + 1}: ${item.question}</p>
+            <p style="color: var(--bodyTx); font-weight: 600;">Q${index + 1}: ${item.question}</p>
             <p style="color: green;">Correct Answer: <span class="correct-answer">${item.correctAnswer} - ${correctAnswerText}</span></p>
             <p style="color: ${item.userAnswer === item.correctAnswer ? 'green' : 'red'};">
                 Your Answer: <span class="${userAnswerClass}">${item.userAnswer} - ${userAnswerText}</span>
